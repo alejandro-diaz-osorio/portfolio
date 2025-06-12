65c9d1b2858e46a1ab5eb3e5fef639581b2634ae
alejandro-diaz-osorio <alejandro.diaz@ucp.edu.co>
2025-06-11 23:42:26 -0500

    feat: update education data with detailed descriptions and correct course order

--- src/data/education.js
+++ src/data/education.js
@@ -4,7 +4,7 @@ export const education = [
     school: "Universidad Católica de Pereira (UCP)",
     period: "2023 - Actualidad",
     description:
-      "Carrera universitaria de pregrado en Ingeniería de Sistemas y Telecomunicaciones, con enfoque en el desarrollo de software y redes.",
+      "Programa orientado a la formación de ingenieros capaces de integrar soluciones tecnológicas en áreas como desarrollo de software, redes de computadoras, seguridad informática y telecomunicaciones, con una visión ética, investigativa y emprendedora.",
   },
   {
     degree: "Inglés B2",
@@ -14,10 +14,24 @@ export const education = [
       "Cursando el nivel C1 de inglés, con experiencia en comunicación oral y escrita.",
   },
   {
-    degree: "SQL (Intermediate)",
+    degree: "Fundamentos de Python 1",
+    school: "CISCO Network Academy",
+    period: "2023",
+    description:
+      "Certificación en Python básico, aprendiendo fundamentos sobre Python, incluyendo introducción a la programación, tipos de datos, entrada y salida, operadores, estructuras de control, listas, funciones, tuplas, diccionarios, manejo de excepciones y operaciones bit a bit.",
+  },
+  {
+    degree: "SQL (Intermedio)",
     school: "HackerRank",
     period: "2024",
     description:
-      "Certificación en SQL intermedio, demostrando habilidades en consultas avanzadas",
+      "Certificación en SQL intermedio, aprendiendo subconsultas (en SELECT, FROM, WHERE), uniones (UNION), joins complejos, agrupaciones.",
+  },
+  {
+    degree: "Excel (Intermedio)",
+    school: "Servicio Nacional de Aprendizaje (SENA)",
+    period: "2021",
+    description:
+      "Certificación en Excel intermedio, demostrando habilidades en manipulación y análisis de datos.",
   },
 ];
