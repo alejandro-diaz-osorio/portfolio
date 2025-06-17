02ad5c1f1c43fb116d5a04f18b33b28e41acc341
alejandro-diaz-osorio <alejandro.diaz@ucp.edu.co>
2025-06-17 00:00:42 -0500

    Update projects
    - Add datacript

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,19 +1,19 @@
 import AlarmaAntirrobo from "../assets/projects/AlarmaAntirrobo.webp";
 import TxRxMorse from "../assets/projects/Tx&RxMorse.webp";
 import BreakOut from "../assets/projects/break_out.webp";
+import DataCRIPT from "../assets/projects/datacript.webp";
 
 export const projects = [
   {
     title: "Alarma Antirrobo",
-    description:
-      "Proyecto universitario elaborado con Arduino y ESP32 con sensor HC-SR04.",
+    description: "Proyecto elaborado con Arduino y ESP32 con sensor HC-SR04 y API de Telegram.",
     link: "https://github.com/alejo-lives/anti-theft-proximity-alarm",
     image: AlarmaAntirrobo,
   },
   {
     title: "Sistema de Comunicación con Códigos Morse",
     description:
-      "Proyecto universitario elaborado con Arduino y ESP32 para enviar mensajes en código Morse.",
+      "Proyecto elaborado con Arduino y ESP32 para enviar mensajes en código Morse.",
     link: "https://github.com/alejo-lives/code-morse-transmissor-and-receiver",
     image: TxRxMorse,
   },
@@ -23,5 +23,12 @@ export const projects = [
       "Juego clásico Break Out desarrollado con HTML5, CSS3 y JavaScript puro sin librerías externas.",
     link: "https://github.com/alejo-lives/BREAK-OUT.git",
     image: BreakOut,
+  },
+  {
+    title: "DATA.CRIPT",
+    description:
+      "Aplicación web para encriptar y desencriptar mensajes usando AES-128 con una clave personalizada.",
+    link: "https://github.com/alejo-lives/datacript.git",
+    image: DataCRIPT,
   },
 ];
