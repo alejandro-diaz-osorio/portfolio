585009d659f03ee0effdfb728ae80470db74e73c
alejandro-diaz-osorio <alejandro.diaz@ucp.edu.co>
2025-06-23 20:39:31 -0500

    feat: add Hora Hispana project and update project details

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,34 +1,42 @@
 import AlarmaAntirrobo from "../assets/projects/AlarmaAntirrobo.webp";
-import TxRxMorse from "../assets/projects/Tx&RxMorse.webp";
+import TxRxMorse from "../assets/projects/Tx&RxMorse.webp"
 import BreakOut from "../assets/projects/break_out.webp";
 import DataCRIPT from "../assets/projects/datacript.webp";
+import Hora_Hispana from "../assets/projects/hora_hispana.webp";
 
 export const projects = [
   {
     title: "Alarma Antirrobo",
     description: "Proyecto elaborado con Arduino y ESP32 con sensor HC-SR04 y API de Telegram.",
-    link: "https://github.com/alejo-lives/anti-theft-proximity-alarm",
+    link: "https://github.com/alejandro-diaz-osorio/anti-theft-proximity-alarm",
     image: AlarmaAntirrobo,
   },
   {
     title: "Sistema de Comunicación con Códigos Morse",
     description:
       "Proyecto elaborado con Arduino y ESP32 para enviar mensajes en código Morse.",
-    link: "https://github.com/alejo-lives/code-morse-transmissor-and-receiver",
+    link: "https://github.com/alejandro-diaz-osorio/code-morse-transmissor-and-receiver",
     image: TxRxMorse,
+  },
+  {
+    title: "DATA.CRIPT",
+    description:
+    "Aplicación web para encriptar y desencriptar mensajes usando AES-128 con una clave personalizada.",
+    link: "https://github.com/alejandro-diaz-osorio/datacript",
+    image: DataCRIPT,
+  },
+  {
+    title: "HORA HISPANA",
+    description:
+    "Aplicación web simple para visualizar la hora en diferentes países de habla hispana a partir de una fecha y hora seleccionada.",
+    link: "https://github.com/alejandro-diaz-osorio/Hora-Hispana",
+    image: Hora_Hispana,
   },
   {
     title: "Break Out",
     description:
       "Juego clásico Break Out desarrollado con HTML5, CSS3 y JavaScript puro sin librerías externas.",
-    link: "https://github.com/alejo-lives/BREAK-OUT.git",
+    link: "https://github.com/alejandro-diaz-osorio/BREAK-OUT",
     image: BreakOut,
-  },
-  {
-    title: "DATA.CRIPT",
-    description:
-      "Aplicación web para encriptar y desencriptar mensajes usando AES-128 con una clave personalizada.",
-    link: "https://github.com/alejo-lives/datacript.git",
-    image: DataCRIPT,
   },
 ];
