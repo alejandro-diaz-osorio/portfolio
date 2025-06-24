a77ae99e27b262b1da2a801e568e404c812baadf
alejandro-diaz-osorio <alejandro.diaz@ucp.edu.co>
2025-06-23 23:51:50 -0500

    feat: update project details and enhance project display with GitHub and demo links

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,35 +8,38 @@ export const projects = [
   {
     title: "Alarma Antirrobo",
     description: "Proyecto elaborado con Arduino y ESP32 con sensor HC-SR04 y API de Telegram.",
-    link: "https://github.com/alejandro-diaz-osorio/anti-theft-proximity-alarm",
+    github: "https://github.com/alejandro-diaz-osorio/anti-theft-proximity-alarm",
     image: AlarmaAntirrobo,
   },
   {
     title: "Sistema de Comunicación con Códigos Morse",
     description:
       "Proyecto elaborado con Arduino y ESP32 para enviar mensajes en código Morse.",
-    link: "https://github.com/alejandro-diaz-osorio/code-morse-transmissor-and-receiver",
+    github: "https://github.com/alejandro-diaz-osorio/code-morse-transmissor-and-receiver",
     image: TxRxMorse,
   },
   {
     title: "DATA.CRIPT",
     description:
-    "Aplicación web para encriptar y desencriptar mensajes usando AES-128 con una clave personalizada.",
-    link: "https://github.com/alejandro-diaz-osorio/datacript",
+    "Aplicación web para encriptar y desencriptar mensajes usando una clave personalizada.",
+    github: "https://github.com/alejandro-diaz-osorio/datacript",
+    demo: "https://datacript.netlify.app/",
     image: DataCRIPT,
   },
   {
     title: "HORA HISPANA",
     description:
     "Aplicación web simple para visualizar la hora en diferentes países de habla hispana a partir de una fecha y hora seleccionada.",
-    link: "https://github.com/alejandro-diaz-osorio/Hora-Hispana",
+    github: "https://github.com/alejandro-diaz-osorio/Hora-Hispana",
+    demo: "https://horahispana.netlify.app/",
     image: Hora_Hispana,
   },
   {
-    title: "Break Out",
+    title: "BREAK-OUT",
     description:
-      "Juego clásico Break Out desarrollado con HTML5, CSS3 y JavaScript puro sin librerías externas.",
-    link: "https://github.com/alejandro-diaz-osorio/BREAK-OUT",
+      "Juego clásico Break-Out desarrollado con HTML5, CSS3 y JavaScript puro sin librerías externas.",
+    github: "https://github.com/alejandro-diaz-osorio/BREAK-OUT",
+    demo: "https://break-out.onrender.com/",
     image: BreakOut,
   },
 ];
