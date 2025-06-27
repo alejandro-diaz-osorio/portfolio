4e870305ace5596b3fbea063a3af5c588d1d0853
alejandro-diaz-osorio <alejandro.diaz@ucp.edu.co>
2025-06-27 02:21:33 -0500

    feat: add additional education entries and update links in Education section

--- src/data/education.js
+++ src/data/education.js
@@ -14,8 +14,17 @@ export const education = [
       "Cursando el nivel C1 de inglés, con experiencia en comunicación oral y escrita.",
   },
   {
+    degree: "Fundamentos de Python 2",
+    school: "CISCO Network Academy | MinTIC",
+    link: "https://www.credly.com/badges/28b761ae-ca37-4044-814f-a9cc8826cf3e/public_url",
+    period: "2025",
+    description:
+      "Certificación en Python intermedio, aprendiendo sobre módulos y paquetes, manejo de cadenas y excepciones, Programación Orientada a Objetos, y herramientas misceláneas como archivos, fechas, sistema operativo y generadores.",
+  },
+  {
     degree: "Fundamentos de Python 1",
-    school: "CISCO Network Academy",
+    school: "CISCO Network Academy | MinTIC",
+    link: "https://www.credly.com/badges/18d37566-9f93-44dd-a25f-e2d4cd520c8f/public_url",
     period: "2023",
     description:
       "Certificación en Python básico, aprendiendo fundamentos sobre Python, incluyendo introducción a la programación, tipos de datos, entrada y salida, operadores, estructuras de control, listas, funciones, tuplas, diccionarios, manejo de excepciones y operaciones bit a bit.",
@@ -23,6 +32,7 @@ export const education = [
   {
     degree: "SQL (Intermedio)",
     school: "HackerRank",
+    link: "https://www.hackerrank.com/certificates/5f857e4bd7a8",
     period: "2024",
     description:
       "Certificación en SQL intermedio, aprendiendo subconsultas (en SELECT, FROM, WHERE), uniones (UNION), joins complejos, agrupaciones.",
@@ -34,4 +44,4 @@ export const education = [
     description:
       "Certificación en Excel intermedio, demostrando habilidades en manipulación y análisis de datos.",
   },
-];
+]
